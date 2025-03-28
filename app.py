b0dfcb728f266d364e14d048202739003b456c95
Shakthi <shakthinandanp0712@gmail.com>
2025-03-28 12:13:43 +0530

    fixed arrows with custom scenes.json (not the generated one)

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('b.html')
+    return render_template('base.html')
 
 if __name__ == '__main__':
     app.run(ssl_context=('cert.pem', 'key.pem'),host="0.0.0.0")
